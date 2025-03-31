fb5e2096d94da174eff62aa14870e71de41a0196
Maria Inês Silva <misilva73@gmail.com>
2025-03-31 14:39:29 +0200

    Speed up aggregation code and add logging

--- src/data/opcodes_agg.py
+++ src/data/opcodes_agg.py
@@ -1,10 +1,18 @@
 import os
 import duckdb
+import logging
+import argparse
+import traceback
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
 
+
 pd.options.mode.chained_assignment = None
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 
 def get_block_height_directories(block_data_dir):
@@ -43,8 +51,8 @@ def get_parquet_path_patterns(block_data_dir):
                     os.path.join(block_height_dir_path, "tx_hash=*/*.parquet")
                 )
             else:
-                block_height = block_height_dir_path.split("/")[-3].split("=")[-1]
-                print(
+                block_height = block_height_dir_path.split("=")[-1]
+                logging.info(
                     f"Skipping block {block_height} because no tx_hash directory found inside."
                 )
     return block_dirs
@@ -63,63 +71,129 @@ def compute_gas_cost_for_chunk(df: pd.DataFrame) -> pd.DataFrame:
     unique_txs = df["tx_hash"].unique()
     new_df = pd.DataFrame()
     for tx_hash in unique_txs:
-        try:
-            tx_df = df[df["tx_hash"] == tx_hash]
-            new_tx_df = compute_gas_costs_for_single_tx(tx_df)
-            new_df = pd.concat([new_df, new_tx_df], ignore_index=True)
-        except Exception as e:
-            print(f"Error at transactions: {tx_hash}")
-            print(f"Exception: {e}")
+        # try:
+        tx_df = df[df["tx_hash"] == tx_hash]
+        new_tx_df = compute_gas_costs_for_single_tx(tx_df)
+        new_df = pd.concat([new_df, new_tx_df], ignore_index=True)
+        # except:
+        # Prints the stacktrace for the exception
+        # So that we can see on which line did the exception occur
+        # logging.error(f"Error at transaction: {tx_hash}")
+        # traceback.print_exc()
     return new_df
 
 
-def compute_gas_costs_for_single_tx(df: pd.DataFrame) -> pd.DataFrame:
+def compute_gas_costs_for_single_tx(initial_df: pd.DataFrame) -> pd.DataFrame:
     """
     Adds a new column to the raw debug trace data (df) with the correct gas cost for the CALL-type opcodes.
     Note that this code assume df only contains data for one transaction
     """
-    call_list = ["DELEGATECALL", "CALL", "CALLCODE", "STATICCALL", "CREATE", "CREATE2"]
-    inner_gas_diff_list = []
-    outer_gas_diff_list = []
-    for i in range(len(df) - 1):
-        row = df.iloc[i]
-        next_row = df.iloc[i + 1]
-        # if the row is a new depth starter
-        if (row["op"] in call_list) and (next_row["depth"] != row["depth"]):
-            # Get the next change of depth that is not a call-type
-            return_row = df[
-                (df["file_row_number"] > row["file_row_number"])
-                & (~df["op"].isin(call_list))
-                & (df["depth"] == row["depth"] + 1)
-            ].iloc[0]
-            # Get and save the available gas at entry and exit
-            ## inside the call
-            inner_entry_gas = next_row["gas"]
-            inner_exit_gas = df[
-                df["file_row_number"] == return_row["file_row_number"]
-            ].iloc[0]["gas"]
-            inner_gas_diff_list.append(inner_entry_gas - inner_exit_gas)
-            ## right outside the call
-            outer_entry_gas = row["gas"]
-            outer_exit_gas = df[
-                df["file_row_number"] == return_row["file_row_number"] + 1
-            ].iloc[0]["gas"]
-            outer_gas_diff_list.append(outer_entry_gas - outer_exit_gas)
-        else:
-            inner_gas_diff_list.append(np.nan)
-            outer_gas_diff_list.append(np.nan)
-    # Compute the correct cost for the call opcodes
-    call_cost_arr = np.array(outer_gas_diff_list + [np.nan]) - np.array(
-        inner_gas_diff_list + [np.nan]
+    df = initial_df.copy()
+    df["has_depth_change"] = df["depth"] != df["depth"].shift(-1)
+    if df["depth"].nunique() == 1:  # if transaction does not have depth changes
+        # There is not need to fix gas cost
+        df["op_gas_cost"] = df["gasCost"]
+        return df
+    else:  # if the transaction has depth changes...
+        call_list = [
+            "DELEGATECALL",
+            "CALL",
+            "CALLCODE",
+            "STATICCALL",
+            "CREATE",
+            "CREATE2",
+        ]
+        inner_gas_diff_list = []
+        outer_gas_diff_list = []
+        change_depth_row_numbers = df[df["has_depth_change"]]["file_row_number"].values
+        filter_row_numbers = np.unique(
+            np.concat(
+                (
+                    change_depth_row_numbers,
+                    change_depth_row_numbers + 1,
+                    change_depth_row_numbers - 1,
+                )
+            )
+        )
+        filter_rows_df = df[df["file_row_number"].isin(filter_row_numbers)].sort_values(
+            "file_row_number"
+        )
+        for i in range(len(filter_rows_df) - 1):
+            row = filter_rows_df.iloc[i]
+            next_row = filter_rows_df.iloc[i + 1]
+            # if the row is a new depth starter
+            if (row["op"] in call_list) and (next_row["depth"] != row["depth"]):
+                # Get the next change of depth that is not a call-type
+                return_row = filter_rows_df[
+                    (filter_rows_df["file_row_number"] > row["file_row_number"])
+                    & (~filter_rows_df["op"].isin(call_list))
+                    & (filter_rows_df["depth"] == row["depth"] + 1)
+                    & (filter_rows_df["has_depth_change"])
+                ].iloc[0]
+                # Get and save the available gas at entry and exit
+                ## inside the call
+                inner_entry_gas = next_row["gas"]
+                inner_exit_gas = filter_rows_df[
+                    filter_rows_df["file_row_number"] == return_row["file_row_number"]
+                ].iloc[0]["gas"]
+                inner_gas_diff_list.append(inner_entry_gas - inner_exit_gas)
+                ## right outside the call
+                outer_entry_gas = row["gas"]
+                outer_exit_gas = filter_rows_df[
+                    filter_rows_df["file_row_number"]
+                    == return_row["file_row_number"] + 1
+                ].iloc[0]["gas"]
+                outer_gas_diff_list.append(outer_entry_gas - outer_exit_gas)
+            else:
+                inner_gas_diff_list.append(np.nan)
+                outer_gas_diff_list.append(np.nan)
+        # Compute the correct cost for the call opcodes
+        call_cost_df = pd.DataFrame(
+            {
+                "file_row_number": filter_rows_df["file_row_number"].values,
+                "op_gas_cost": np.array(outer_gas_diff_list + [np.nan])
+                - np.array(inner_gas_diff_list + [np.nan]),
+            }
+        )
+        # Merge with original df
+        df = df.merge(call_cost_df, on="file_row_number", how="left").drop(
+            columns="has_depth_change"
+        )
+        df["op_gas_cost"] = np.where(
+            df["op_gas_cost"].isna(), df["gasCost"], df["op_gas_cost"]
+        )
+        return df
+
+
+def parse_configuration():
+    src_dir = os.path.dirname(os.path.abspath(__file__))
+    parser = argparse.ArgumentParser(
+        description="Loads data from EVM debug traces calculates the correct gas costs and aggregates the results."
     )
-    df["op_gas_cost"] = np.where(df["op"].isin(call_list), call_cost_arr, df["gasCost"])
-    return df
+    parser.add_argument(
+        "--data_dir",
+        type=str,
+        default=os.path.abspath(os.path.join(src_dir, "..", "..", "data")),
+        help="Data directory (default: ./data). Used for both input and output.",
+    )
+    parser.add_argument(
+        "--reprocess",
+        type=bool,
+        default=False,
+        help="Whether to reprocessed blocks. Default is False.",
+    )
+    args = parser.parse_args()
+    config = {
+        "data_dir": args.data_dir,
+        "reprocess": args.reprocess,
+    }
+    return config
 
 
 def main():
+    config = parse_configuration()
     # Directories
-    src_dir = os.path.dirname(os.path.abspath(__file__))
-    data_dir = os.path.abspath(os.path.join(src_dir, "..", "..", "data"))
+    data_dir = config["data_dir"]
     block_data_dir = os.path.join(data_dir, "block_data")
     block_dirs = get_parquet_path_patterns(block_data_dir)
     # block_dirs now contains paths to parquet files in valid block_height directories
@@ -139,9 +213,10 @@ def main():
             f"block_range={block_range}",
         )
         output_file_path = os.path.join(output_dir, "file.parquet")
-        if os.path.isfile(output_file_path):
-            print(f"Block range {block_range} is already processed. Skipping...")
+        if os.path.isfile(output_file_path) and config["reprocess"] == False:
+            logging.info(f"Block range {block_range} is already processed. Skipping...")
             continue
+        logging.info(f"Start processing block range {block_range} ...")
         # Define query
         query = f"""
         SELECT block_height,
@@ -181,50 +256,5 @@ def main():
         df.to_parquet(output_file_path)
 
 
-def main_test(block_height: str, tx_hash: str = "*"):
-    # Paths
-    src_dir = os.path.dirname(os.path.abspath(__file__))
-    block_dir = os.path.abspath(os.path.join(src_dir, "..", "..", "data", "block_data"))
-    filename = os.path.join(
-        block_dir, f"block_height={block_height}", f"tx_hash={tx_hash}", "file.parquet"
-    )
-    # Query
-    query = f"""
-        SELECT block_height,
-            tx_hash,
-            file_row_number,
-            op,
-            gas,
-            gasCost,
-            depth
-        FROM read_parquet(
-                '{ filename }',
-                hive_partitioning = TRUE,
-                filename = True,
-                file_row_number = True,
-                union_by_name = True
-            );
-        """
-    # Execute the query and convert the result to a DataFrame
-    raw_df = duckdb.query(query).to_df()
-    # Fix issues with gas costs
-    clean_df = compute_gas_cost_for_chunk(raw_df)
-    # Aggregate data for memory efficiency
-    df = (
-        clean_df.groupby(["block_height", "tx_hash", "op", "op_gas_cost"])
-        .size()
-        .reset_index()
-    )
-    df.columns = [
-        "block_height",
-        "tx_hash",
-        "op",
-        "op_gas_cost",
-        "op_gas_pair_count",
-    ]
-    df.to_parquet("debug_df.parquet")
-
-
 if __name__ == "__main__":
-    block_height = "22000040"
-    main_test(block_height)
+    main()
